b7c4c19d68f1e4dfb812d30a94eb6900bb3cd25e
johnfrankmorgan <johnfrankmorgan@gmail.com>
2023-12-28 11:35:20 +0000

    make tests less noisy

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 )
 
 func init() {
+	level := slog.Level(0)
+
+	if env, ok := os.LookupEnv("CHESTER_LOG_LEVEL"); ok {
+		if err := level.UnmarshalText([]byte(env)); err != nil {
+			fmt.Fprintf(os.Stderr, "invalid log level %q, defaulting to debug\n", env)
+			level = slog.LevelDebug
+		}
+	}
+
 	slog.SetDefault(
 		slog.New(
 			slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 				AddSource: true,
-				Level:     slog.LevelDebug,
+				Level:     level,
 			}),
 		),
 	)
